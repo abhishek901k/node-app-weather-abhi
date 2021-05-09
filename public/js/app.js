1b983de733ae0269609c7ce52fda8b4bcbf566c7
unknown <abhishek901k.com>
2021-05-09 10:51:12 +0530

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log('Client Side javascript file loaded!');
+//console.log('Client Side javascript file loaded!');
 
 fetch('http://puzzle.mead.io/puzzle').then((response)=>{
     response.json().then((data)=>{
